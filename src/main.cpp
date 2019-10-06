366963968d38d63d2e8960ca76e6013f8f251eb2
MisterGC <serein.pfeiffer@gmail.com>
2019-10-06 12:01:01 +0200

    Add options to enable profiling easily.

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+//Activate for debugging/profiling
+//#include <QQmlDebuggingEnabler>
+//static QQmlDebuggingEnabler enable;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
